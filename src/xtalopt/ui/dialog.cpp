b936d752ce09246de08efe5cd6b7497f89b0d97b
David Lonie <loniedavid@gmail.com>
2010-06-04 14:06:39 -0400

    Removed debugging input

--- src/xtalopt/ui/dialog.cpp
+++ src/xtalopt/ui/dialog.cpp
@@ -354,7 +354,6 @@ namespace Avogadro {
   }
 
   void XtalOptDialog::repaintProgressBar_() {
-    //qDebug() << "XtalOptDialog::repaintProgressBar_: Val: " << ui.progbar->value() << " Min: " << ui.progbar->minimum() << " Max: " << ui.progbar->maximum();
     ui.label_prog->repaint();
     ui.progbar->repaint();
   }
