4a953fe3e4fadbc74adbd094433443da0d9833ac
Patrick Avery <psavery@buffalo.edu>
2016-01-14 23:19:34 -0500

    Changed licensing info for spgInit program.

--- src/xtalopt/spgInit/elemInfo.h
+++ src/xtalopt/spgInit/elemInfo.h
@@ -2,16 +2,16 @@
   elemInfoDatabase.h - Contains the database information needed in elemInfo.h
                        Contains atomic symbol and atomic radii information
 
-  Copyright (C) 2016 by Patrick S. Avery
+  Copyright (C) 2015 - 2016 by Patrick S. Avery
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation version 2 of the License.
+  This source code is released under the New BSD License, (the "License").
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
  ***********************************************************************/
 
 #ifndef ELEM_INFO_H
