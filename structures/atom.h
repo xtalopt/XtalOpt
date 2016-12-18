13af9850406b289402ef08b61e78eadda547be5d
Patrick Avery <psavery@buffalo.edu>
2016-12-18 17:41:03 -0500

    Created atom, molecule, and unit cell classes
    This is a contribution to making XtalOpt independent.
    In addition to creating these classes, I am trying to get
    us to move away from using pointers. These classes still
    need to be tested, but they can sooner or later be incorporated
    into XtalOpt.

---
+++ structures/atom.h
@@ -0,0 +1,103 @@
+
+
+#ifndef XTALOPT_ATOM_H
+#define XTALOPT_ATOM_H
+
+#include "vector.h"
+
+namespace XtalOpt
+{
+
+ /**
+  * @class Atom atom.h
+  * @brief A basic atom class. Each atom has an atomic number and a position.
+  */
+  class Atom
+  {
+   public:
+    /**
+     * Atom constructor. Default atomic number is 0. Default position
+     * is (0, 0, 0).
+     *
+     * @param atomicNum The atomic number. Default is zero.
+     * @param pos The 3-dimensional Cartesian coordinates in Angstroms.
+     *            Default is (0, 0, 0).
+     */
+    explicit Atom(unsigned char atomicNum = 0,
+                  const Vector3& pos = Vector3(0.0, 0.0, 0.0));
+
+    /* Copy constructor. Just copies the data. */
+    Atom(const Atom& other);
+
+    /* Assignment operator. Just copies the data. */
+    Atom& operator=(const Atom& other);
+
+    /* Comparison operator. Just compares the data. */
+    bool operator==(const Atom& other) const;
+
+    /**
+     * Set the atomic number of the atom. It may be any number between 0 and
+     * 255. However, real atoms will be be between 1 and 118.
+     *
+     * @param num The atomic number.
+     */
+    void setAtomicNumber(unsigned char num) { m_atomicNumber = num; };
+
+    /**
+     * Set the position of the atom using Cartesian coordinates in Angstroms.
+     *
+     * @param pos The 3-dimensional Cartesian coordinates in Angstroms.
+     */
+    void setPos(const Vector3& pos) { m_pos = pos; };
+
+    /**
+     * Get the atomic number of the atom.
+     *
+     * @return The atomic number of the atom.
+     */
+    unsigned char atomicNumber() const { return m_atomicNumber; };
+
+    /**
+     * Get the position of the atom in 3-dimensional Cartesian coordinates
+     * in Angstroms.
+     *
+     * @return The 3-dimensional Cartesian position in Angstroms.
+     */
+    Vector3 pos() const { return m_pos; };
+
+   private:
+    unsigned char m_atomicNumber;
+    Vector3 m_pos;
+  };
+
+  inline Atom::Atom(unsigned char atomicNum, const Vector3& pos)
+    : m_atomicNumber(atomicNum),
+      m_pos(pos)
+  {
+  }
+
+  inline Atom::Atom(const Atom& other)
+    : m_atomicNumber(other.m_atomicNumber),
+      m_pos(other.m_pos)
+  {
+  }
+
+  inline Atom& Atom::operator=(const Atom& other)
+  {
+    m_atomicNumber = other.m_atomicNumber;
+    m_pos = other.m_pos;
+    return *this;
+  }
+
+  inline bool Atom::operator==(const Atom& other) const
+  {
+    if (m_atomicNumber == other.m_atomicNumber &&
+        m_pos == other.m_pos) {
+      return true;
+    }
+    return false;
+  }
+
+}
+
+#endif
---
+++ structures/test.cpp
@@ -0,0 +1,9 @@
+
+#include "molecule.h"
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello!\n";
+}
