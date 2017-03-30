39c20080fcd128cefd26da4d4ddd1a11e9466f77
Patrick Avery <psavery@buffalo.edu>
2017-03-30 17:00:46 -0400

    Added headers to include all QI and Opt headers

---
+++ src/globalsearch/queueinterfaces/queueinterfaces.h
@@ -0,0 +1,26 @@
+/**********************************************************************
+  queueinterfaces.h - Just includes all the queue interface header files.
+
+  Copyright (C) 2017 by Patrick S. Avery
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+ ***********************************************************************/
+
+#ifndef GLOBALSEARCH_QUEUEINTERFACES_H
+#define GLOBALSEARCH_QUEUEINTERFACES_H
+
+#include <globalsearch/queueinterfaces/loadleveler.h>
+#include <globalsearch/queueinterfaces/local.h>
+#include <globalsearch/queueinterfaces/lsf.h>
+#include <globalsearch/queueinterfaces/pbs.h>
+#include <globalsearch/queueinterfaces/sge.h>
+#include <globalsearch/queueinterfaces/slurm.h>
+
+#endif
---
+++ src/xtalopt/optimizers/optimizers.h
@@ -0,0 +1,25 @@
+/**********************************************************************
+  optimizers.h - Just includes all the optimizer header files.
+
+  Copyright (C) 2017 by Patrick S. Avery
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+ ***********************************************************************/
+
+#ifndef XTALOPT_OPTIMIZERS_H
+#define XTALOPT_OPTIMIZERS_H
+
+#include <xtalopt/optimizers/castep.h>
+#include <xtalopt/optimizers/gulp.h>
+#include <xtalopt/optimizers/pwscf.h>
+#include <xtalopt/optimizers/siesta.h>
+#include <xtalopt/optimizers/vasp.h>
+
+#endif
