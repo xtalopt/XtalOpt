cdb4f16de8899e4def1ca25e279b42cdf9721e3c
Patrick Avery <psavery@buffalo.edu>
2017-07-03 17:23:22 -0400

    Officially changed license to 3-clause BSD

--- src/globalsearch/optimizer.cpp
+++ src/globalsearch/optimizer.cpp
@@ -3,14 +3,13 @@
 
   Copyright (C) 2010-2011 by David C. Lonie
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation version 2 of the License.
+  This source code is released under the New BSD License, (the "License").
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
  ***********************************************************************/
 
 #include <globalsearch/optimizer.h>
