15f2e2becc9efc012010bca156afcaacbcb5e963
Patrick Avery <psavery@buffalo.edu>
2017-07-22 16:41:50 -0400

    Fixed faulty md5 generation for AppVeyor

--- scripts/write-md5.bat
+++ scripts/write-md5.bat
@@ -3,11 +3,9 @@
 REM Windows batch files take a lot of code to do simple things
 
 REM Find the package name and set it in a variable
-for /r %%i in (.\*) do (
-  if /i [%i:~-10%]==[-win32.exe] (
-    set @packagename=%%i
-    goto :break
-  )
+for %%i in (.\*-win32.exe) do (
+  set packagename=%%i
+  goto :break
 )
 
 :break
@@ -20,6 +18,8 @@ set row=
 for /f "skip=1 delims=*" %%a in (windows-xtalopt.md5) do (
   if defined row echo.!row! >> tmpFile.txt
   set row=%%a
+  REM delete the spaces in row
+  set row=!row: =!
 )
 xcopy tmpFile.txt windows-xtalopt.md5 /y
 del tmpFile.txt /f /q
