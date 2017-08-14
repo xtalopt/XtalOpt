c4549e243610465a9ddd31e8eed62d135be41421
Patrick Avery <psavery@buffalo.edu>
2017-08-14 14:14:32 -0400

    Change license title in tests to BSD

--- tests/structuretest.cpp
+++ tests/structuretest.cpp
@@ -3,15 +3,13 @@
 
   Copyright (C) 2010 David C. Lonie
 
-  XtalOpt is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  This source code is released under the New BSD License, (the "License").
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-  02110-1301, USA.
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
  **********************************************************************/
 
 #include <globalsearch/structure.h>
