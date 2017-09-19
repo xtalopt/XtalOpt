565c9e28917be06f28da127c4708c144aea8c0df
Patrick Avery <psavery@buffalo.edu>
2017-09-19 00:41:49 -0400

    First try to get Molecular XtalOpt to compile with MSVC 2017

---
+++ cmake/modules/DepSearchDirs.cmake
@@ -0,0 +1,22 @@
+# ssh.dll should be in dir(${LIBSSH_LIBRARIES})\..\bin
+get_filename_component(LIBSSH_DLL_DIR "${LIBSSH_LIBRARIES}" DIRECTORY)
+set(DEP_SEARCH_DIRS "${DEP_SEARCH_DIRS}" "${LIBSSH_DLL_DIR}/../bin")
+
+# qwt.dll should be in the same dir as ${QWT_LIBRARIES}
+get_filename_component(QWT_DLL_DIR "${QWT_LIBRARIES}" DIRECTORY)
+set(DEP_SEARCH_DIRS "${DEP_SEARCH_DIRS}" "${QWT_DLL_DIR}")
+
+# All of the Qt dependencies will hopefully be together in the bin of the
+# root directory. We will need to change this part in the future if they
+# are not.
+get_target_property(QtCore_location Qt5::Core LOCATION)
+get_filename_component(QtCore_location "${QtCore_location}" DIRECTORY)
+set(DEP_SEARCH_DIRS "${DEP_SEARCH_DIRS}" "${QtCore_location}/../bin")
+
+# All of the Boost dependencies will hopefully be in the boost library dirs
+string(REGEX REPLACE "\\\\" "/" _tmp "${Boost_LIBRARY_DIRS}")
+set(DEP_SEARCH_DIRS "${DEP_SEARCH_DIRS}" "${_tmp}")
+
+# All of the RDKit dependencies will hopefully be in the RDKit library dirs
+string(REGEX REPLACE "\\\\" "/" _tmp "${RDKit_LIBRARY_DIRS}")
+set(DEP_SEARCH_DIRS "${DEP_SEARCH_DIRS}" "${_tmp}")
