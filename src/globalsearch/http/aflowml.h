5143a94d32537bbba5192f0d51186ac4916927fe
Patrick Avery <psavery@buffalo.edu>
2017-11-28 14:59:17 -0500

    Update copyright notice on the http classes

--- src/globalsearch/http/aflowml.h
+++ src/globalsearch/http/aflowml.h
@@ -1,3 +1,18 @@
+/**********************************************************************
+  AflowML - Submit a machine learning calculation to the AFLOW server and
+            get the results
+
+  Copyright (C) 2017-2018 by Patrick Avery
+
+  This source code is released under the New BSD License, (the "License").
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ ***********************************************************************/
+
 #ifndef AFLOWML_H
 #define AFLOWML_H
 
