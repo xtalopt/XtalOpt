be171dde7a51365c104fbc0d8e3c1ed565e037b2
Patrick Avery <psavery@buffalo.edu>
2017-12-02 16:18:41 -0500

    Moved most of travis-ci scripts to script files

---
+++ scripts/travis/after_success.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+# For GCC only: capture coverage info, filter out system, print debug info,
+# and upload to codecov
+if [[ "$NAME" == "gcc-4.8" ]]; then
+  lcov --directory . --capture --output-file coverage.info
+  lcov --remove coverage.info '/usr/*' --output-file coverage.info
+  lcov --list coverage.info
+  bash <(curl -s https://codecov.io/bash) || \
+  echo "Codecov did not collect coverage reports"
+fi
+
+# If this is a tag, then zip the install directory, md5sum it, and upload it
+if [[ -n "$TRAVIS_TAG" ]] && [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+  if [[ "$NAME" == "gcc-4.8" ]]; then
+    echo "Tag detected for gcc-4.8. Installing, zipping installation, and creating md5sum."
+    make -j2 install
+    cd install
+    tar -czvf linux-xtalopt.tgz xtalopt
+    md5sum linux-xtalopt.tgz > linux-xtalopt.md5
+  elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    echo "Tag detected for osx. Installing, zipping installation, and creating md5sum."
+    sudo make -j2 install
+    sudo chown -R travis ./install
+    cd install/xtalopt
+    zip -r osx-xtalopt.zip xtalopt.app
+    md5 osx-xtalopt.zip > osx-xtalopt.md5
+  fi
+else
+  echo "Tag not detected. Skipping zip and md5sum."
+fi
---
+++ scripts/travis/script.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+# Before script
+# For Linux, we have to load the travis-ci display to run the tests even
+# though we don't actually need the display...
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  CMAKE_FLAGS="$CMAKE_FLAGS -DCMAKE_PREFIX_PATH=/usr/local/opt/qt/"
+  CMAKE_FLAGS="$CMAKE_FLAGS -DQWT_INCLUDE_DIR=/usr/local/opt/qwt/lib/qwt.framework/Headers"
+else
+  export DISPLAY=:99.0
+  sh -e /etc/init.d/xvfb start
+  sleep 3
+  CMAKE_FLAGS="$CMAKE_FLAGS -DENABLE_MOLECULAR=ON"
+fi
+
+CMAKE_FLAGS="$CMAKE_FLAGS -DBUILD_INDEPENDENT_PACKAGE=ON"
+CMAKE_FLAGS="$CMAKE_FLAGS -DCMAKE_INSTALL_PREFIX=install/xtalopt"
+CMAKE_FLAGS="$CMAKE_FLAGS -DBUILD_TESTS=ON"
+mkdir build
+cd build
+mkdir install
+cmake ${CMAKE_FLAGS} ..
+
+# Script
+make -j2
+ctest --output-on-failure
