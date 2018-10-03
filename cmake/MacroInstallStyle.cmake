ea9d8cc9f953781da471172d0f1cadf0b0eec417
Patrick Avery <psavery@buffalo.edu>
2018-10-03 17:38:13 -0500

    Install styles directory for Windows also

--- cmake/MacroInstallStyle.cmake
+++ cmake/MacroInstallStyle.cmake
@@ -1,9 +1,15 @@
 # Written by Patrick Avery - 2018
 # This is a macro for installing a Qt style during a build
-# Currently, it is only used for Apple and qmacstyle
+# Currently, it is used for qmacstyle and qwindowsvistastyle
 
 macro(InstallQMacStyle _destination _qt_plugins_var)
   include(MacroInstallQt5Plugin)
   install_qt5_plugin("Qt5::QMacStylePlugin"
+                     "${_destination}" "${_qt_plugins_var}")
+endmacro()
+
+macro(InstallQWindowsVistaStyle _destination _qt_plugins_var)
+  include(MacroInstallQt5Plugin)
+  install_qt5_plugin("Qt5::QWindowsVistaStylePlugin"
                      "${_destination}" "${_qt_plugins_var}")
 endmacro()
