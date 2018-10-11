f09516a841dfe3f430fe66569b91914522baa33d
Patrick Avery <psavery@buffalo.edu>
2018-10-11 18:25:39 -0400

    Results.txt file: print "duplicate of" structure
    Before, we would not show which structure is a duplicate of which
    in the results.txt file. It would just say "duplicate" or "supercell".
    
    Now, it actually prints out which structure it is a
    duplicate/supercell of.

--- src/xtalopt/structures/xtal.cpp
+++ src/xtalopt/structures/xtal.cpp
@@ -2200,10 +2200,10 @@ QString Xtal::getResultsEntry(bool includeHardness) const
       status = "Killed";
       break;
     case Duplicate:
-      status = "Duplicate";
+      status = "Duplicate (" + getDuplicateString() + ")";
       break;
     case Supercell:
-      status = "Supercell";
+      status = "Supercell (" + getSupercellString() + ")";
       break;
     case Error:
       status = "Error";
@@ -2226,7 +2226,7 @@ QString Xtal::getResultsEntry(bool includeHardness) const
       .arg(getEnthalpy() / static_cast<double>(getFormulaUnits()), 12)
       .arg(getFormulaUnits(), 4)
       .arg(m_spgSymbol, 11)
-      .arg(status, 12);
+      .arg(status, 22);
   }
   else {
     return QString("%1 %2 %3 %4 %5 %6 %7 %8")
@@ -2237,7 +2237,7 @@ QString Xtal::getResultsEntry(bool includeHardness) const
       .arg(getFormulaUnits(), 4)
       .arg(vickersHardness(), 10)
       .arg(m_spgSymbol, 11)
-      .arg(status, 12);
+      .arg(status, 22);
   }
 }
 
