a55519c329f714eafa1c3b442938e498d0c5bab1
Patrick Avery <patrick.avery@kitware.com>
2019-06-17 10:48:17 -0400

    Bump version numbers to 12.1
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- src/xtalopt/molecularmain.cpp
+++ src/xtalopt/molecularmain.cpp
@@ -63,7 +63,7 @@ int main(int argc, char* argv[])
   QCoreApplication::setOrganizationName("XtalOpt");
   QCoreApplication::setOrganizationDomain("xtalopt.github.io");
   QCoreApplication::setApplicationName("XtalOpt");
-  QCoreApplication::setApplicationVersion("12.0");
+  QCoreApplication::setApplicationVersion("12.1");
 
   QCommandLineParser parser;
   parser.setApplicationDescription("XtalOpt: an open-source evolutionary "
